cab99a2b9f72b6e92b3c1abceb3fe943eacad5c1
Aravinda Bhat <aravindabhat@protonmail.com>
2019-07-08 10:40:58 +0000

    >> Supports Random [str, num, ascii, spec]

--- index.js
+++ index.js
@@ -1,18 +1,85 @@
-module.exports = function randomStr(length = 1, caseType = 'lower') {
-    if(length < 0) {
-        throw new RangeError("Can't Process the request");
-    }
-    
-    if(length > 1e6) {
-        return new RangeError("I dont want to blow up your system...!");
-    }
+module.exports = function generate(type='string', length=1, uppercase=false) {
+    try {
+        if(type.toLower() == 'alphabets') {
+            return (uppercase ? randomAlphabets(length).toUpperCase() : randomAlphabets(length))  
+        }
+        
+        if(type == 'number') {
+            return randomNumber(length)
+        }
 
-    let randomString = '';
-    
-    for (let i = 0; i < length; i++) {
-        let random = Math.floor(Math.random() * 97) + 124;
-        randomString += String.fromCharCode(random);
-    } 
-    
-    return ( caseType == 'upper' ? randomString.toUpperCase() : randomString);
+        if(type.toLower() == 'string') {
+            let result = '';
+            for (let i = 0; i < length; i++) {
+                let chooseFrom = Math.floor(Math.random() * 2) * 1;
+                switch (chooseFrom) {
+                    case 0: result +=randomAlphabets();
+                        break;
+                    case 1: result +=randomNumber();
+                        break;
+                    default: result += 'a'
+                }   
+            }
+            return (uppercase ? result.toUpperCase() : result);
+        }
+
+        if(type.toLower() == 'asciimix') {
+            return (uppercase ? randomAsciiChar(length).toUpperCase() : randomAsciiChar(length))  
+        }
+
+        if(type.toLower() == 'special') {
+
+        }
+
+        throw new Error(`unrecognized argument "${type}"`)
+    } catch (error) {
+        throw new Error(`${error}`)
+    }
+}
+
+function randomAlphabets(length = 1) {
+    if(length > 1) {
+        let result = '';
+        for (i = 0; i < length; i++) {
+            result += String.fromCharCode(Math.floor(Math.random() * 26) + 97);
+        }
+
+        return result;
+    }
+    return String.fromCharCode(Math.floor(Math.random() * 27) + 97);
+}
+
+function randomNumber(length) {
+    if(length > 1) {
+        let result = '';
+        for (i = 0; i < length; i++) {
+            result += String.fromCharCode(Math.floor(Math.random() * 9) + 48);
+        }
+        return result;
+    }
+    return String.fromCharCode(Math.floor(Math.random() * 9) + 48);
+}
+
+function randomAsciiChar(length = 1) {
+    if(length > 1) {
+        let result = '';
+        for (i = 0; i < length; i++) {
+            result += String.fromCharCode(Math.floor(Math.random() * 94) + 33);
+        }
+
+        return result;
+    }
+    return String.fromCharCode(Math.floor(Math.random() * 94) + 33);
+}
+
+
+function randomSpecialChar(length) {
+    if(length > 1) {
+        let result = '';
+        for (i = 0; i < length; i++) {
+            result += String.fromCharCode(Math.floor(Math.random() * 9) + 48);
+        }
+        return result;
+    }
+    return String.fromCharCode(Math.floor(Math.random() * 9) + 48);
 }
