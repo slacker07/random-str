64d34ba573e2e064f2c7a0cc09492bcd11def18c
Aravinda Bhat <mobistreak@mrbhatscbookpro.lan>
2019-07-23 16:19:22 +0530

    Qualify it as clean error handling?

---
+++ examples/generate.js
@@ -0,0 +1,12 @@
+const randomStr = require('../index');
+
+console.log(randomStr('string', 3, false))
+
+console.log(randomStr('number', 3, false))
+
+console.log(randomStr('alphabet', 3, false))
+
+console.log(randomStr('string', 3, false))
+
+console.log(randomStr('speciasgdfl', 1, false))
+
--- index.js
+++ index.js
@@ -1,56 +1,66 @@
 // Importing array of special characters
 const { special } = require('./char');
+const chalk = require('chalk')
+
+class UnrecognizedArgument extends Error {
+    constructor(message) {
+        super(message);
+        this.name = "UnrecognizedArgumentError";
+    }
+}
 
 // Main export 
-module.exports = function generate(type='string', length=1, uppercase=false) {
+module.exports = function generate(type = 'string', length = 1, uppercase = false) {
     try {
 
-        if(length > 1e6 || length < 1) {
-            throw `Length range of boundry \n Max ${1e6}, Min 1`;
+        if (length > 1e6 || length < 1) {
+            throw new RangeError(chalk.bold(`Length range out of boundry (Max ${1e6}, Min 1)`));
         }
 
-        if(type.toLowerCase() == 'alphabet') {
-            return (uppercase ? randomAlphabets(length).toUpperCase() : randomAlphabets(length))  
+        if (type.toLowerCase() == 'alphabet') {
+            return (uppercase ? randomAlphabets(length).toUpperCase() : randomAlphabets(length))
         }
-        
-        if(type == 'number') {
+
+        if (type == 'number') {
             return randomNumber(length)
         }
 
-        if(type.toLowerCase() == 'string') {
+        if (type.toLowerCase() == 'string') {
             let result = '';
             for (let i = 0; i < length; i++) {
                 let chooseFrom = Math.floor(Math.random() * 2) * 1;
                 switch (chooseFrom) {
-                    case 0: result +=randomAlphabets();
+                    case 0: result += randomAlphabets();
                         break;
-                    case 1: result +=randomNumber();
+                    case 1: result += randomNumber();
                         break;
                     default: result += 'a'
-                }   
+                }
             }
             return (uppercase ? result.toUpperCase() : result);
         }
 
-        if(type.toLowerCase() == 'asciimix') {
-            return (uppercase ? randomAsciiChar(length).toUpperCase() : randomAsciiChar(length))  
+        if (type.toLowerCase() == 'asciimix') {
+            return (uppercase ? randomAsciiChar(length).toUpperCase() : randomAsciiChar(length))
         }
 
-        if(type.toLowerCase() == 'special') {
+        if (type.toLowerCase() == 'special') {
             return randomSpecialChar(length)
         }
 
-        throw new Error(`unrecognized argument "${type}"`)
+        throw new UnrecognizedArgument(`unrecognized argument "${type}"`)
     } catch (error) {
-        throw new Error(`${error}`)
+        // throw new Error(`${error}`)
+        const message = `--= ${chalk.yellow(chalk.bold(error.name))} :: ${chalk.red(error.message)} =-- \n${error.stack}`
+        return message;
     }
 }
 
 function randomAlphabets(length = 1) {
-    if(length > 1e6 || length < 1) {
+    if (length > 1e6 || length < 1) {
         throw new RangeError(`Length range of boundry \n Max ${1e6}, Min 1`)
     }
-    if(length > 1) {
+    if (length > 1) {
         let result = '';
         for (i = 0; i < length; i++) {
             result += String.fromCharCode(Math.floor(Math.random() * 26) + 97);
@@ -62,10 +72,10 @@ function randomAlphabets(length = 1) {
 }
 
 function randomNumber(length) {
-    if(length > 1e6 || length < 1) {
+    if (length > 1e6 || length < 1) {
         throw new RangeError(`Length range of boundry \n Max ${1e6}, Min 1`)
     }
-    if(length > 1) {
+    if (length > 1) {
         let result = '';
         for (i = 0; i < length; i++) {
             result += String.fromCharCode(Math.floor(Math.random() * 9) + 48);
@@ -76,10 +86,10 @@ function randomNumber(length) {
 }
 
 function randomAsciiChar(length = 1) {
-    if(length > 1e6 || length < 1) {
+    if (length > 1e6 || length < 1) {
         throw new RangeError(`Length range of boundry \n Max ${1e6}, Min 1`)
     }
-    if(length > 1) {
+    if (length > 1) {
         let result = '';
         for (i = 0; i < length; i++) {
             result += String.fromCharCode(Math.floor(Math.random() * 94) + 33);
@@ -92,10 +102,10 @@ function randomAsciiChar(length = 1) {
 
 
 function randomSpecialChar(length) {
-    if(length > 1e6 || length < 1) {
+    if (length > 1e6 || length < 1) {
         throw new RangeError(`Length range of boundry \n Max ${1e6}, Min 1`)
     }
-    if(length > 1) {
+    if (length > 1) {
         let result = '';
         for (i = 0; i < length; i++) {
             result += special[Math.floor(Math.random() * 32)];
@@ -110,25 +120,26 @@ function generatePattern(uppercase = false, ...pattern) {
         const patternArray = [...pattern];
         let result = '';
         for (pattern of patternArray) {
-            if(/\ds/.test(pattern)) {
+            if (/\ds/.test(pattern)) {
                 result += generate('string', pattern.match(/\d+/)[0])
             }
-            if(/\da/.test(pattern)) {
+            if (/\da/.test(pattern)) {
                 result += randomAlphabets(pattern.match(/\d+/)[0])
             }
-            if(/\dn/.test(pattern)) {
+            if (/\dn/.test(pattern)) {
                 result += randomNumber(pattern.match(/\d+/)[0])
             }
-            if(/\dam/.test(pattern)) {
+            if (/\dam/.test(pattern)) {
                 result += randomAsciiChar(pattern.match(/\d+/)[0])
             }
-            if(/\dsp/.test(pattern)) {
+            if (/\dsp/.test(pattern)) {
                 result += randomSpecialChar(pattern.match(/\d+/)[0])
             }
         }
 
-        return ( uppercase ? result.toUpperCase() : result)
-    } catch (e) {
-        console.log(e);
+        return (uppercase ? result.toUpperCase() : result)
+    } catch (error) {
+        const message = `--= ${chalk.yellow(chalk.bold(error.name))} :: ${chalk.red(error.message)} =-- \n${error.stack}`
+        return message;
     }
 }
